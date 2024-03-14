2ed524bcbddbe396e5d3908ce1214ece1c1b99e8
Your Name <you@example.com>
2024-03-14 21:26:16 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -84,8 +84,15 @@ y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
 */
 int checkSorted(int arr[], int size) 
 {
-  
-  
+  int variable = -2;
+  for (int i = 0; i < size - 1; i++)
+  {
+    if (arr[i] > arr[i + 1])
+    {
+      vairable = -1;
+    }
+  }
+
   return -2; 
 }
 
